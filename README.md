e8ba8a9bd7f1840a2bb38f806d74e38ef3e661c2
dzuberi <dzuberi522@gmail.com>
2025-08-14 02:26:33 +0000

    updated to multi-bit signals across multiple files.

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 Converts VCD files to PWL files. WIP.
 
-Dependency: https://github.com/SanDisk-Open-Source/pyvcd/
+Dependencies:
+- https://github.com/SanDisk-Open-Source/pyvcd/
+- https://github.com/PyHDI/Pyverilog
+
+# `vcd2pwl.py`
+
+This script is able to take in a VCD file and generate analog equivalents for SPICE stimulus
+
+# `create_stimulus.py`
+
+This script is able to parse port definitions from equivalent SPICE and Verilog files to determine which stimulus to generate. The end goal is to have it generate SPICE stimulus which uses the correct PWL files and prints the correct outputs
--- vcd2pwl.py
+++ vcd2pwl.py
@@ -32,6 +32,19 @@ class Value:
     def __init__(self, value, time):
         self.value = value
         self.time = time
+        if type(self.value) is str:
+            assert(all(v in ['0', '1', 'x', 'z'] for v in self.value)), \
+                f"Value must be a string of '0', '1', 'x', or 'z', got {self.value}"
+    def get_bit_value(self, bit):
+        if type(self.value) is str:
+            if len(self.value) == 1:
+                return Value(self.value, self.time)
+            else: 
+                return Value(self.value[-1 - bit], self.time)
+        else:
+            print(f"Error: Value {self.value} is not a string")
+            raise TypeError("Value must be a string of '0', '1', 'x', or 'z'")
+
     def copy(self):
         return Value(self.value, self.time)
     def __str__(self):
@@ -90,37 +103,51 @@ class PWLConverter:
 
     def convert_dvt_to_analog(self):
         self.analog_value_table = defaultdict(list)
-        for id, values in self.digital_value_table.items():
-            time = values[0].time * self.vcd.timescale
-            value = self.digital_to_analog(values[0].value)
-            if value != self.Z:
-                self.analog_value_table[id].append(Value(value, time))
-            for i in range(1, len(values)):
-                if values[i].value == values[i-1].value:
-                    continue
-                time = values[i].time * self.vcd.timescale
-                prev_value = self.digital_to_analog(values[i-1].value)
-                new_value = self.digital_to_analog(values[i].value)
-                if prev_value != self.Z:
-                    self.analog_value_table[id].append(Value(prev_value, time))
-                if new_value != self.Z:
-                    self.analog_value_table[id].append(Value(new_value, time + self.TRF))
+        for id, all_values in self.digital_value_table.items():
+            assert(all([signal_width == self.vcd.symbol_table[id][0].size for signal_width in [s.size for s in self.vcd.symbol_table[id]]]))
+            signal_width = self.vcd.symbol_table[id][0].size
+            # print(f"Converting signal {id} with width {signal_width} to analog values.")
+            # signal_width = self.vcd.symbol_table[id].size
+            for bit in range(signal_width):
+                # print(f"Processing bit {bit} of signal {id}: {(id, bit)}.")
+                values = [value.get_bit_value(bit) for value in all_values]
+                time = values[0].time * self.vcd.timescale
+                value = self.digital_to_analog(values[0].value)
+                if value != self.Z:
+                    self.analog_value_table[(id, bit)].append(Value(value, time))
+                # else:
+                #     print(f"Warning: Initial value for signal {id} bit {bit} is Z. Skipping initial value.")
+                for i in range(1, len(values)):
+                    if values[i].value == values[i-1].value:
+                        continue
+                    time = values[i].time * self.vcd.timescale
+                    prev_value = self.digital_to_analog(values[i-1].value)
+                    new_value = self.digital_to_analog(values[i].value)
+                    if prev_value != self.Z:
+                        self.analog_value_table[(id, bit)].append(Value(prev_value, time))
+                    if new_value != self.Z:
+                        self.analog_value_table[(id, bit)].append(Value(new_value, time + self.TRF))
     
     def dump_pwls(self, pwl_dir="pwls", submodule="prga_tb_top.i_postimpl.dut"):
         os.makedirs(pwl_dir, exist_ok=True)
-        file_to_id ={i: id for i, id in enumerate(self.vcd.symbol_table.keys())}
+        # print(sorted(list(self.analog_value_table.keys()), key=lambda x: x[1], reverse=False))
+        file_to_id ={i: id for i, id in enumerate(self.analog_value_table.keys())}
         id_to_file = {id: i for i, id in file_to_id.items()}
         data = {
             'id_to_signal': self.vcd.symbol_table,
             'file_to_id': file_to_id
         }
         pickle.dump(data, open(os.path.join(pwl_dir, "dicts.pickle"), "wb"))
-        for id, values in tqdm(sorted(self.analog_value_table.items(), key = lambda x: len(x[1]), reverse = True)):
+        for id, values in tqdm(self.analog_value_table.items(), desc="Writing PWL files", unit="file"):
             file_name = f"{id_to_file[id]}.pwl"
             file_path = os.path.join(pwl_dir, file_name)
+            # import pdb; pdb.set_trace()
+            # if self.vcd.symbol_table[id[0]][0].size > 1:
+                # print(f"Writing {file_path} with {len(values)} values for key {id} corresponding to signal {self.vcd.symbol_table[id[0]][0].name} with length {self.vcd.symbol_table[id[0]][0].size}.")
             with open(file_path, "w") as fid:
                 for value in values:
                     fid.write(f"{value.time} {value.value}\n")
+        print(f"Dumped {len(self.analog_value_table)} PWL files to {pwl_dir}. Total size: {get_dir_size_du(pwl_dir)}")
         
 
 class VCDIngest:
@@ -146,7 +173,6 @@ class VCDIngest:
 
     def add_signal(self, type, size, id, name):
         signal_path = '.'.join(self.current_scope + [name])
-        # print(f"Signal added: {signal_path} (Type: {type}, Size: {size}, ID: {id})")
         self.symbol_table[id].append(Signal(type, size, id, signal_path))
         self.value_table[id] = [] # default value is an empty list because the initial values are set in the VCD file at least in Icarus
     
@@ -165,8 +191,6 @@ class VCDIngest:
                 scope = token.scope
                 match scope.type_:
                     case pyvcd.ScopeType.module:
-                        # print(f"Module scope: {scope.ident}")
-                        # print(f"Current scope: {'.'.join(self.current_scope)}")
                         self.current_scope.append(scope.ident)
                     case pyvcd.ScopeType.task:
                         print(f"Task scope: {scope.ident}")
@@ -212,51 +236,60 @@ class VCDIngest:
             case pyvcd.TokenKind.UPSCOPE:
                 if self.current_scope:
                     self.current_scope.pop()
-                    # print(f"Upscoped to {'.'.join(self.current_scope)}")
                 else:
                     raise ValueError("No current scope to pop.")
             case pyvcd.TokenKind.VAR:
-                type = str(token.var.type_)
+                type_ = str(token.var.type_)
                 size = token.var.size
                 identifier = token.var.id_code
                 reference = token.var.reference
-                self.add_signal(type, size, identifier, reference)
-                # raise NotImplementedError("VAR token handling not implemented.")
+                self.add_signal(type_, size, identifier, reference)
             case pyvcd.TokenKind.VERSION: #TODO: Does this do anything?
                 print(token.version)
                 pass
             case pyvcd.TokenKind.DUMPALL: #TODO: Does this do anything?
                 pass
-                # raise NotImplementedError("DUMPALL token handling not implemented.")
             case pyvcd.TokenKind.DUMPOFF: #TODO: Does this do anything?
                 pass
-                # raise NotImplementedError("DUMPOFF token handling not implemented.")
             case pyvcd.TokenKind.DUMPON: #TODO: Does this do anything?
                 pass
-                # raise NotImplementedError("DUMPON token handling not implemented.")
             case pyvcd.TokenKind.DUMPVARS: #TODO: Does this do anything?
                 pass
-                # raise NotImplementedError("DUMPVARS token handling not implemented.")
             case pyvcd.TokenKind.END:
-                # raise NotImplementedError("END token handling not implemented.")
                 pass
             case pyvcd.TokenKind.CHANGE_TIME:
                 self.time = token.time_change
-                # print(f"Time changed to {self.time}")
-                # input("Press Enter to continue...")
-                # raise NotImplementedError("CHANGE_TIME token handling not implemented.")
             case pyvcd.TokenKind.CHANGE_SCALAR:
                 id_code = token.scalar_change.id_code
                 value = token.scalar_change.value
+                if type(value) is not str:
+                    # convert to binary string representation
+                    value = bin(value)[2:].zfill(self.symbol_table[id_code][0].size)
+                assert(type(value) is str)
+                assert (len(value) == self.symbol_table[id_code][0].size or len(value) == 1), \
+                    f"Value length mismatch: value='{value}', len(value)={len(value)}, expected size={self.symbol_table[id_code][0].size}, id_code={id_code}, signal={self.symbol_table[id_code][0].name}"
                 self.value_table[id_code].append(Value(value, self.time))
-                # print(f"Scalar change: {id_code} = {value}")
-                # raise NotImplementedError("CHANGE_SCALAR token handling not implemented.")
             case pyvcd.TokenKind.CHANGE_VECTOR:
                 id_code = token.vector_change.id_code
                 value = token.vector_change.value
+                # print(f"Processing vector change for {id_code}, {self.symbol_table[id_code][0].name}, size: {self.symbol_table[id_code][0].size} with value {value} at time {self.time}.")
+                if type(value) is not str:
+                    # convert to binary string representation
+                    value = bin(value)[2:].zfill(self.symbol_table[id_code][0].size)
+                # this fix is necessary because if there is an x in the value, it will come as a string and might not match the size
+                if len(value) != self.symbol_table[id_code][0].size:
+                    # extend the value with 'x' to match the size
+                    # extending 'x' is really inconsequential because it will be converted to 0 in the PWL
+                    if value[0] == 'x':
+                        value = 'x' * (self.symbol_table[id_code][0].size - len(value)) + value
+                    # extend the value with '0' to match the size
+                    elif value[0] == '0' or value[0] == '1':
+                        value = '0' * (self.symbol_table[id_code][0].size - len(value)) + value
+                # assertions for sanity
+                assert(type(value) is str)
+                assert (len(value) == self.symbol_table[id_code][0].size or len(value) == 1), \
+                    f"Value length mismatch: value='{value}', len(value)={len(value)}, expected size={self.symbol_table[id_code][0].size}, id_code={id_code}, signal={self.symbol_table[id_code][0].name}"
                 self.value_table[id_code].append(Value(value, self.time))
-                # print(f"Vector change: {id_code} = {value}")
-                # raise NotImplementedError("CHANGE_VECTOR token handling not implemented.")
             case pyvcd.TokenKind.CHANGE_REAL: #TODO: Not seen in Icarus VCD
                 print(token.change_real)
                 raise NotImplementedError("CHANGE_REAL token handling not implemented.")
